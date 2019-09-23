33f6321c9bf1841d577406f8a3557f9a764f6a71
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-23 00:45:15 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,8 +6,8 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kid, age|
-    puts 
-  # add your code snippet here!
+    puts "Happy Birthday #{kid}! You are now #{age} years old!"
+  end
 end
 
 
